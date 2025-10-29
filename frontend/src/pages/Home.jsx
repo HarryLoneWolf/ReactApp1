9abc139b55ba826dbbf6031550ef2dc1d51e52d5
HarryLoneWolf <haroonhussain2007@gmail.com>
2025-10-29 19:40:53 +0000

    added:     -content to Favorites     -useState to Home     -added react-router-dom     -created and added navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home() {
+        const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "John Wick", release_date: '2014' },
         { id: 2, title: "Iron Man", release_date: '2008' },
@@ -9,19 +12,35 @@ function Home() {
         { id: 5, title: "Gran Turismo", release_date: '2023' },
     ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    };
 
     return(
         
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text"  placeholder="Search for movies..." className="search-input"/>
+
+                <input 
+                    type="text"  
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+
+                <button type="submit" className="search-button">Search</button>
+
             </form>
 
             <div className="movies-grid">
-                {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
+                {movies.map(
+                    (movie) =>
+                        (
+                        <MovieCard movie={movie} key={movie.id} />
+                        )
+                )}
 
             </div>
         </div>
