a16e220bc5aaf2f6c8018827abb34e6952e6684c
HarryLoneWolf <haroonhussain2007@gmail.com>
2025-10-29 21:28:10 +0000

    implemented API and started on react context

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,47 @@
 import '../css/Home.css'
 import MovieCard from "../components/MovieCard"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from '../services/api'
 
 function Home() {
-        const [searchQuery, setSearchQuery] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "John Wick", release_date: '2014' },
-        { id: 2, title: "Iron Man", release_date: '2008' },
-        { id: 3, title: "The Avengers", release_date: '2012' },
-        { id: 4, title: "F1", release_date: '2025' },
-        { id: 5, title: "Gran Turismo", release_date: '2023' },
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies);
+            }catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            } 
+            finally {
+                setLoading(false)
+            }
+        }
 
-    const handleSearch = (e) => {
+        loadPopularMovies()
+    }, [])
+
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
+
+        if (!searchQuery.trim())return
+        if (loading) return
+
+        setLoading(true)
+        try{
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        }catch (err) {
+            setError('Failed to search movies...')
+        }finally{
+            setLoading(false)
+        }
     };
 
     return(
@@ -35,15 +61,17 @@ function Home() {
 
             </form>
 
-            <div className="movies-grid">
-                {movies.map(
-                    (movie) =>
-                        (
-                        <MovieCard movie={movie} key={movie.id} />
-                        )
-                )}
+                {error && <div className='error-message'>{error}</div>}
 
-            </div>
+            {loading ? (
+            <div className="loading">Loading...</div>
+            ) : (
+                <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+                </div>
+            )}
         </div>
     );
 }
